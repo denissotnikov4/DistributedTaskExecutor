ac8b1cd63a9b4d224bcaf4f8e56df97bc873b701
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2025-12-21 02:10:00 +0500

    работа с env значениями

--- src/Core/Core/Database/DatabaseOptions.cs
+++ src/Core/Core/Database/DatabaseOptions.cs
@@ -2,15 +2,5 @@ namespace Core.Database;
 
 public class DatabaseOptions
 {
-    public string Name { get; set; } = null!;
-
-    public string User { get; set; } = null!;
-
-    public string Password { get; set; } = null!;
-
-    public string Host { get; set; } = null!;
-
-    public string Port { get; set; } = null!;
-
-    public string GetConnectionString() => $"Host={Host};Port={Port};Database={Name};Username={User};Password={Password}";
+    public string ConnectionString { get; set; } = null!;
 }
