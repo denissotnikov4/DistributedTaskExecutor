da6a423f01985571aadbce0f76a5e56e7b6a8c9b
luvairo-m <grigory.sav3lev@yandex.ru>
2025-12-25 15:56:27 +0500

    Добавил тесты на выполнение кода

---
+++ src/WorkerService/WorkerService.Tests/CodeExecution/CSharpCodeExecutionTests.cs
@@ -0,0 +1,39 @@
+﻿using TaskService.Client.Models.Tasks;
+using ExecutionContext = WorkerService.Cli.Models.ExecutionContext;
+
+namespace WorkerService.Tests.CodeExecution;
+
+[TestFixture]
+public class CSharpCodeExecutionTests : CodeExecutionTestsBase
+{
+    protected override ExecutionContext GetContextFor_ExecuteCodeWithInput_Success()
+    {
+        return new ExecutionContext
+        {
+            Name = Guid.NewGuid().ToString(),
+            Code = "public static class Program { public static void Main() { var input = int.Parse(Console.In.ReadToEnd()); Console.Write(input + 1); } }",
+            Input = "41",
+            Language = ProgrammingLanguage.CSharp
+        };
+    }
+
+    protected override ExecutionContext GetContextFor_ExecuteCodeWithoutInput_Success()
+    {
+        return new ExecutionContext
+        {
+            Name = Guid.NewGuid().ToString(),
+            Code = "public static class Program { public static void Main() { Console.Write(42); } }",
+            Language = ProgrammingLanguage.CSharp
+        };
+    }
+
+    protected override ExecutionContext GetContextFor_ExecuteCode_Error()
+    {
+        return new ExecutionContext
+        {
+            Name = Guid.NewGuid().ToString(),
+            Code = "public static class Program { public static void Main() { throw new Exception(); } }",
+            Language = ProgrammingLanguage.CSharp
+        };
+    }
+}
